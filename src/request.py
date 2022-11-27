ea12963b719dafcd0e5e971c73566e2b82ba544b
Pashkov K.V <pk69@kruger.su>
2022-11-27 20:36:20 +0700

    Update request.py

--- src/request.py
+++ src/request.py
@@ -8,12 +8,12 @@ import json
 
 class req1C:
      def __init__(self, c_shop,nConfig):
-          mConfig = nConfig
+          self.mConfig = nConfig
 
      def __enter__(self):
           return self
 
-     def __exit__(self, exc_type, exc_val, exc_tb):
+     def __exit__(self):
           self.close()
           
      def exeQuery(self):
@@ -21,7 +21,7 @@ class req1C:
                r = requests.get('http://' + self.mConfig._sections.one_C.server_ip + ':' 
                                    + self.mConfig._sections.one_C.port 
                                    + self.mConfig._sections.one_C.lquery)
-               #r = requests.get('http://192.168.252.250:8082/UNF_test/hs/test_s/V1/test')
+# ?  r = requests.get('http://192.168.252.250:8082/UNF_test/hs/test_s/V1/test')
                r.encoding = 'utf-8' 
                print(r.status_code)
                c_count = r.json()
